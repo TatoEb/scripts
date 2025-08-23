2e37c205035d4dc7515b1fee0f448f88c2c937a8
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-08-23 08:03:48 +0300

    Update x100+p.sh

--- x100+p.sh
+++ x100+p.sh
@@ -39,7 +39,7 @@ git clone https://github.com/TatoEb/adss-x100.git
 cd adss-x100
 chmod +x *.sh
 
-# Копіюємо файли у $HOME
+# Копіюємо файли з подальшим очищенням
 cd ~
 cp ~/adss-x100/*.* ~
 rm -rf x100-for-docker adss-x100
