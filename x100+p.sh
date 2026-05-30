11a6c4d1e34c4750595ef7067b49ed3e725e1956
TatoEb <109128185+TatoEb@users.noreply.github.com>
2026-05-30 22:54:25 +0300

    Update x100+p.sh

--- x100+p.sh
+++ x100+p.sh
@@ -4,7 +4,7 @@ set -e  # зупиняти скрипт при помилках
 USER_ID="$1"
 
 if [ -z "$USER_ID" ]; then
-    echo "❌ Помилка: потрібно вказати itArmyUserId як аргумент!"
+    echo "❌ Помилка: потрібно вказати statisticsUserId як аргумент!"
     echo "   Приклад запуску: ./x100+p.sh 1278306477"
     exit 1
 fi
@@ -57,7 +57,7 @@ sed -i -E "
   s/maxDistressScale=10240/maxDistressScale=100000/;
   s/delayAfterSessionMinDuration=15/delayAfterSessionMinDuration=0/;
   s/delayAfterSessionMaxDuration=45/delayAfterSessionMaxDuration=2/;
-  s/itArmyUserId=77777777/itArmyUserId=${USER_ID}/;
+  s/statisticsUserId=77777777/statisticsUserId=${USER_ID}/;
   s/fixedVpnConnectionsQuantity=0/fixedVpnConnectionsQuantity=7/;
   s/networkUsageGoal=80%/networkUsageGoal=777/;
   s/oneSessionMinDuration=600/oneSessionMinDuration=400/;
